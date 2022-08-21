58ed7274922645a6d2130dd46fe4a2150c80cb16
jaeder <zxb360@gmail.com>
2022-08-21 19:34:01 -0300

    Primeiros passos javascript-variaveis

---
+++ node.js
@@ -0,0 +1,27 @@
+// Variaveis são que nem caixas de memorias, vou explicar na pratica;
+// 1 - Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+
+const myName = 'seu nome dentro das ASPA';  //PALAVRA CHAVE P/CONSTANTE 'CONST'
+
+// 2 - Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+
+const birthCity = 'Minha cidade';
+
+// 3 - Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+
+let birthYear = 1950;   //VARIAVEIS TIPO NUMBER NÃO É NECESSARIO AS
+
+// 4 - Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+
+console.log(myName, birthCity, birthYear, 'Pode-se chamar todas as variaveis console.log');
+//ou
+console.log(myName)
+console.log(birthCity)
+
+// 5 - Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+
+birthYear = 2030;
+
+// 6 - Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro?
+
+birthCity = 'ELSalvador' //O erro acontece porquê a variavel é uma constante, e as constantes não pode ser mudadas
